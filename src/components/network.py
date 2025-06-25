0c7a50e08c845ea55b87e80fc04e353edd793523
Arthur Negrão <arthurnegrao10s@gmail.com>
2025-06-25 16:37:22 -0300

    Updated network.py: added node_type and start_seed

--- src/components/network.py
+++ src/components/network.py
@@ -5,18 +5,20 @@ import networkx as nx
 
 from .topologies import TopologyGen
 
-from typing import Type
+from typing import Type, Union
 
 
 class Network:
-    def __init__(self) -> None:
+    def __init__(self, topology: str, node_type: str, start_seed: Union[None, int] = None) -> None:
         self.timeline: Timeline = Timeline()
-        self.topology: str
-        self.graph: nx.Graph
-        self.nodes: dict[int, Type[Node]]
+       
         self.number_of_nodes: int
+        self.nodes: dict[int, Type[Node]]
         self.bsm_nodes: dict[tuple[int, int], BSMNode]
-        self.topology_generator = TopologyGen(self)
+        
+        self.topology: str = topology
+        self.graph: nx.Graph
+        self.topology_generator = TopologyGen(self, node_type=node_type, start_seed=start_seed)
 
     def draw(self, labels: bool = True) -> None:
         """
