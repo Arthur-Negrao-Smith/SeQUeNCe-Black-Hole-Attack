bda1dd795171bd2596fb6676369c83ea72bf1c54
Arthur Negrão <arthurnegrao10s@gmail.com>
2025-06-26 17:29:25 -0300

    Was changed the typehints and circular import

--- src/components/nodes.py
+++ src/components/nodes.py
@@ -1,9 +1,9 @@
 from sequence.kernel.timeline import Timeline
 from sequence.topology.node import Node
 from sequence.components.memory import Memory
-from sequence.protocol import Message, Protocol
+from sequence.protocol import Protocol
+from sequence.message import Message
 
-from .managers import RepeaterManager
 from .utils.constants import MEMORY_FIDELITY, MEMORY_FREQUENCY, MEMORY_EFFICIENCY, MEMORY_COHERENCE_TIME, MEMORY_WAVELENGTH
 
 from typing import Type
@@ -12,23 +12,24 @@ class QuantumRepeater(Node):
     """
     Quantum Router to increase the entanglement range on the Network
     """
-    def __init__(self, name: str, tl: Timeline, swap_prob: float) -> None:
-        super().__init__(name, tl)
+    def __init__(self, name: str, timeline: Timeline, swap_prob: float) -> None:
+        super().__init__(name, timeline)
 
         self.left_memo_name: str = f'{name}.left_memo'
         self.right_memo_name: str = f'{name}.right_memo'
 
-        left_memo: Memory = Memory(self.left_memo_name, tl, MEMORY_FIDELITY, MEMORY_FREQUENCY, MEMORY_EFFICIENCY, MEMORY_COHERENCE_TIME, MEMORY_WAVELENGTH)
-        right_memo: Memory = Memory(self.right_memo_name, tl, MEMORY_FIDELITY, MEMORY_FREQUENCY, MEMORY_EFFICIENCY, MEMORY_COHERENCE_TIME, MEMORY_WAVELENGTH)
+        left_memo: Memory = Memory(self.left_memo_name, timeline, MEMORY_FIDELITY, MEMORY_FREQUENCY, MEMORY_EFFICIENCY, MEMORY_COHERENCE_TIME, MEMORY_WAVELENGTH)
+        right_memo: Memory = Memory(self.right_memo_name, timeline, MEMORY_FIDELITY, MEMORY_FREQUENCY, MEMORY_EFFICIENCY, MEMORY_COHERENCE_TIME, MEMORY_WAVELENGTH)
         
         self.add_component(left_memo)
         self.add_component(right_memo)
 
+        from .managers import RepeaterManager
         self.resource_manager: RepeaterManager = RepeaterManager(self)
 
         self.swap_prob: float = swap_prob
 
-    def receive_message(self, src: str, msg: Type[Message]) -> None:
+    def receive_message(self, src: str, msg: Message) -> None:
         self.protocols[0].received_message(src, msg)
 
     def run_protocol(self) -> None:
