9e71665214689d9ce8f9948f4a3a6ff07ee7a3b8
Arthur Negrão <arthurnegrao10s@gmail.com>
2025-06-26 17:27:30 -0300

    Was added typehint to Directions Enum; Changed create_entanglement_protocol function to accept Directions

--- src/components/managers.py
+++ src/components/managers.py
@@ -4,7 +4,8 @@ from sequence.components.memory import Memory
 from sequence.entanglement_management.swapping import EntanglementSwappingA, EntanglementSwappingB
 from sequence.entanglement_management.generation import EntanglementGenerationA
 
-from .nodes import QuantumRepeater
+from components.nodes import QuantumRepeater
+
 from .utils.constants import SWAP_DEGRADATION
 from .utils.enums import Directions
 
@@ -15,8 +16,8 @@ class BaseManager(ABC):
     """
     Base Manager to create to entanglement swapping managers
     """
-    def __init__(self, owner: Node) -> None:
-        self.owner: Node = owner
+    def __init__(self, owner: QuantumRepeater) -> None:
+        self.owner: QuantumRepeater = owner
         self.raw_counter: int = 0
         self.ent_counter: int = 0
 
@@ -32,7 +33,8 @@ class RepeaterManager(BaseManager):
     """
     Manager to control entanglement and entanglement swapping in repeater nodes
     """
-    def __init__(self, owner: Type[QuantumRepeater]) -> None:
+    from .nodes import QuantumRepeater
+    def __init__(self, owner: QuantumRepeater) -> None:
         super().__init__(owner)
 
     def create_swapping_protocolA(self) -> None:
@@ -49,12 +51,12 @@ class RepeaterManager(BaseManager):
                                                         SWAP_DEGRADATION)
         self.owner.protocols.append(protocol)
         
-    def create_swapping_protocolB(self, memory_position: str) -> None:
+    def create_swapping_protocolB(self, memory_position: Directions) -> None:
         """
         Create a protocol to perform the entanglement swapping at the side node.
 
         Args:
-            memory_position (str): Position of the memory to entanglement (left or right)
+            memory_position (Directions): Position of the memory to entanglement (left or right)
         """
         if memory_position == Directions.LEFT:
             left_memo: Memory = self.owner.components[self.owner.left_memo_name]
@@ -66,14 +68,19 @@ class RepeaterManager(BaseManager):
             protocol: EntanglementSwappingB = EntanglementSwappingB(self.owner, f'{self.owner.name}.Entanglement_SwappingB', right_memo)
             self.owner.protocols.append(protocol)
 
-    def create_entanglement_protocol(self, middle_node: str, other_node: str) -> None:
+    def create_entanglement_protocol(self, memory_position: Directions, middle_node: str, other_node: str) -> None:
         """
         Create a protocol to perform the entanglement at the side node.
 
         Args:
+            memory_positions (Directions): Position of the memory to entanglement (left or right)
             middle_node (str): Name of the BSMNode that will generate the entanglement.
             other_node (str): Name of the RepeaterNode that will be entangled.
         """
-        protocol: EntanglementGenerationA = EntanglementGenerationA(self.owner, f"{self.owner.name}.Entanglement_GenerationA", middle_node, other_node,
-                                                                     self.owner.components[self.memo_name])
+        if memory_position == Directions.LEFT:
+            memory: Memory = self.owner.components[self.owner.left_memo_name]
+        else:
+            memory: Memory = self.owner.components[self.owner.right_memo_name]
+        protocol: EntanglementGenerationA = EntanglementGenerationA(self.owner, f"{self.owner.name}.Entanglement_GenerationA", 
+                                                                    middle_node, other_node, memory)
         self.owner.protocols.append(protocol)
