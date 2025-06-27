6d198aed04f36da0461df95d84742b4113e31434
Arthur Negrão <arthurnegrao10s@gmail.com>
2025-06-27 01:42:19 -0300

    Added docstrings and logging

--- src/components/network.py
+++ src/components/network.py
@@ -3,25 +3,43 @@ from sequence.topology.topology import Node, BSMNode
 
 import networkx as nx
 
+from components.network_manager import Network_Manager
 from components.nodes import QuantumRepeater
 from components.utils.enums import Topologies
 
-from typing import Type, Union
+from typing import Optional
+import logging
 
+log: logging.Logger = logging.getLogger(__name__)
 
 class Network:
-    def __init__(self, topology: Topologies, start_seed: Union[None, int] = None) -> None:
+    """
+    Network class to create the quantum network
+    """
+    def __init__(self, topology: Topologies, start_seed: Optional[int] = None) -> None:
+        """
+        Network constructor
+
+        Args:
+            topology (Topologies): Topology to build
+            start_seed (Optional[int]): Seed to replicate the simulation. Default is None
+        """
         from .topologies import TopologyGen
         
         self.timeline: Timeline = Timeline()
        
         self.number_of_nodes: int
-        self.nodes: dict[int, QuantumRepeater]
-        self.bsm_nodes: dict[tuple[int, int], BSMNode]
+        self.nodes: dict[int, QuantumRepeater] = dict()
+        self.black_holes: dict[int, QuantumRepeater] = dict()
+        self.bsm_nodes: dict[tuple[int, int], BSMNode] = dict()
         
         self.topology: Topologies = topology
         self.graph: nx.Graph
         self.topology_generator = TopologyGen(self, start_seed=start_seed)
+
+        self.network_manager: Network_Manager = Network_Manager(self)
+
+        log.debug("Initiated Network")
 
     def draw(self, labels: bool = True) -> None:
         """
