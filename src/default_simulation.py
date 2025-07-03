4fef6170c0fc29104de080f37f748037bb782930
Arthur Negrão <arthurnegrao10s@gmail.com>
2025-07-03 11:06:01 -0300

    feat(simulation): Add data manager to collect network's data.

--- src/default_simulation.py
+++ src/default_simulation.py
@@ -1,9 +1,12 @@
+from components.data_manager import Data_Manager
 from components.network import Network
 from components.simulations import AsyncSimulator
 
 from random import choice
 from copy import copy
 from datetime import datetime
+
+FILENAME: str = '/home/negrao/Documentos/Sequence/SeQUeNCe-Black-Hole-Attack/src/data/default_simulation/default_simulation.json'
 
 # Topology constants
 ROWS: int = 3
@@ -14,7 +17,10 @@ TARGETS: list[int] = [0, 1]
 BLACK_HOLES_NUMBER: list[int] = [1, 3, 5]
 INTENSITIES: list[float] = [0.1*i for i in range(1, 8)]
 
-def simulation(runs: int, resquests_per_run: int, attempts_per_request: int) -> dict:
+def simulation(runs: int, resquests_per_run: int, attempts_per_request: int) -> Data_Manager:
+
+    all_data: Data_Manager = Data_Manager()
+    all_data.load_json(filename=FILENAME, create_file_if_not_exist=True)
 
     # Run without black holes
     for run in range(runs):
@@ -33,6 +39,10 @@ def simulation(runs: int, resquests_per_run: int, attempts_per_request: int) -> 
             
             network.network_manager.request(nodeA_id=nodeA_id, nodeB_id=nodeB_id, 
                                             max_attempts_per_entanglement=attempts_per_request, max_request_attempts=2)
+
+        all_data.update_data(network.network_data)
+        all_data.insert_data_in_json(element_key=run, keys=['no-black-hole'])
+        all_data.write_json(filename=FILENAME)
 
     # run simulations with black holes
     for target in TARGETS:
@@ -56,5 +66,13 @@ def simulation(runs: int, resquests_per_run: int, attempts_per_request: int) -> 
                         network.network_manager.request(nodeA_id=nodeA_id, nodeB_id=nodeB_id, 
                                                         max_attempts_per_entanglement=attempts_per_request, max_request_attempts=2)
                         
+                    all_data.update_data(network.network_data)
+                    all_data.insert_data_in_json(element_key=run, keys=[target, bh_number, intensity])
+                    all_data.write_json(filename=FILENAME)
+                        
         
-    return dict()
+    return all_data
+
+
+sim = AsyncSimulator(simulation_function=simulation, runs=10, cores=3)
+sim.run(100, 2)
