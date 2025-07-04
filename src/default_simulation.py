43f80f818e0c4f2726f1c9ecd9a78918b40f2ce3
Arthur Negrão <arthurnegrao10s@gmail.com>
2025-07-03 23:02:25 -0300

    fix(simulation): Fix the simulation's data organize and fit to use a id.
    Now the simulation write correct json data.
    Now the simulation use a process_id to identify the file writed.

--- src/default_simulation.py
+++ src/default_simulation.py
@@ -4,9 +4,8 @@ from components.simulations import AsyncSimulator
 
 from random import choice
 from copy import copy
-from datetime import datetime
 
-FILENAME: str = '/home/negrao/Documentos/Sequence/SeQUeNCe-Black-Hole-Attack/src/data/default_simulation/default_simulation.json'
+PATH: str = 'src/data/default_simulation'
 
 # Topology constants
 ROWS: int = 3
@@ -17,10 +16,12 @@ TARGETS: list[int] = [0, 1]
 BLACK_HOLES_NUMBER: list[int] = [1, 3, 5]
 INTENSITIES: list[float] = [0.1*i for i in range(1, 8)]
 
-def simulation(runs: int, resquests_per_run: int, attempts_per_request: int) -> Data_Manager:
+def simulation(runs: int, process_id: int, resquests_per_run: int, attempts_per_request: int) -> Data_Manager:
+
+    filename: str = f"{PATH}/default_simulation_{process_id}.json"
 
     all_data: Data_Manager = Data_Manager()
-    all_data.load_json(filename=FILENAME, create_file_if_not_exist=True)
+    # all_data.load_json(filename=filename, create_file_if_not_exist=False)
 
     # Run without black holes
     for run in range(runs):
@@ -36,13 +37,13 @@ def simulation(runs: int, resquests_per_run: int, attempts_per_request: int) -> 
             tmp_nodes.remove(nodeA_id)
 
             nodeB_id: int = choice(tmp_nodes)
-            
+
             network.network_manager.request(nodeA_id=nodeA_id, nodeB_id=nodeB_id, 
                                             max_attempts_per_entanglement=attempts_per_request, max_request_attempts=2)
 
         all_data.update_data(network.network_data)
         all_data.insert_data_in_json(element_key=run, keys=['no-black-hole'])
-        all_data.write_json(filename=FILENAME)
+        all_data.write_json(filename=filename)
 
     # run simulations with black holes
     for target in TARGETS:
@@ -62,17 +63,17 @@ def simulation(runs: int, resquests_per_run: int, attempts_per_request: int) -> 
                         tmp_nodes.remove(nodeA_id)
 
                         nodeB_id: int = choice(tmp_nodes)
-                        
+
                         network.network_manager.request(nodeA_id=nodeA_id, nodeB_id=nodeB_id, 
                                                         max_attempts_per_entanglement=attempts_per_request, max_request_attempts=2)
-                        
+
                     all_data.update_data(network.network_data)
                     all_data.insert_data_in_json(element_key=run, keys=[target, bh_number, intensity])
-                    all_data.write_json(filename=FILENAME)
-                        
-        
+                    all_data.write_json(filename=filename)
+
+
     return all_data
 
 
-sim = AsyncSimulator(simulation_function=simulation, runs=10, cores=3)
-sim.run(100, 2)
+sim = AsyncSimulator(simulation_function=simulation, runs=10, cores=3, need_id=True)
+sim.run(100, 8)
