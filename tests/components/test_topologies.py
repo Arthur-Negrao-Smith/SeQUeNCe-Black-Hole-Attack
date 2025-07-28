4962abf67355fe2506b8df090bf8047200d5c490
Arthur Negrão <arthurnegrao10s@gmail.com>
2025-07-28 11:45:02 -0300

    test: Add tests to test reproducibility of topologies with a seed.

--- tests/components/test_topologies.py
+++ tests/components/test_topologies.py
@@ -12,6 +12,7 @@ class Test_TopologyGen:
     number_of_nodes: int = 9
     prob_edge_creation: float = 0.5
     edges_to_attach: int = 3
+    seed_test: int = 0
 
 
     def test_grid_topology_number_of_nodes_and_topology_name(self, network: Network) -> None:
@@ -67,3 +68,25 @@ class Test_TopologyGen:
         network.topology_generator.destroy()
 
         assert network.topology_generator.network is None
+
+
+    def test_reproducibility_of_erdos_renyi(self) -> None:
+
+        network_1: Network = Network(start_seed=self.seed_test)
+        network_1.topology_generator.erdos_renyi_topology(number_of_nodes=self.number_of_nodes, prob_edge_creation=self.prob_edge_creation)
+
+        network_2: Network = Network(start_seed=self.seed_test)
+        network_2.topology_generator.erdos_renyi_topology(number_of_nodes=self.number_of_nodes, prob_edge_creation=self.prob_edge_creation)
+
+        assert network_1.edges() == network_2.edges()
+
+
+    def test_reproducibility_of_barabasi_albert(self) -> None:
+
+        network_1: Network = Network(start_seed=self.seed_test)
+        network_1.topology_generator.barabasi_albert_topology(number_of_nodes=self.number_of_nodes, edges_to_attach=self.edges_to_attach)
+
+        network_2: Network = Network(start_seed=self.seed_test)
+        network_2.topology_generator.barabasi_albert_topology(number_of_nodes=self.number_of_nodes, edges_to_attach=self.edges_to_attach)
+
+        assert network_1.edges() == network_2.edges()
