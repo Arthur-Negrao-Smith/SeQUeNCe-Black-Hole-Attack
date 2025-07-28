3cbf09dc9c114ac44ba381d955c7257159579f28
Arthur Negrão <arthurnegrao10s@gmail.com>
2025-07-28 11:33:10 -0300

    test: Add tests to test the reproducibility of Attack Manager.

--- tests/components/test_attack_manager.py
+++ tests/components/test_attack_manager.py
@@ -11,6 +11,16 @@ def network() -> Network:
 
     return network
 
+def network_with_seed() -> Network:
+    start_seed: int = 0
+
+    number_of_nodes: int = 10
+    network: Network = Network(start_seed=start_seed)
+    network.topology_generator.line_topology(number_of_nodes)
+
+    return network
+
+
 
 class Test_Attack_Manager:
     number_of_black_holes: int = 4
@@ -57,3 +67,39 @@ class Test_Attack_Manager:
         network.attack_manager.destroy()
 
         assert network.attack_manager.network is None
+
+
+    def test_reproducibility_for_black_hole_without_targets_creation(self) -> None:
+
+        network_1: Network = network_with_seed()
+        network_1.attack_manager.create_black_holes(number_of_black_holes=self.number_of_black_holes, swap_prob=self.swap_prob, targets_per_black_hole=0)
+
+        network_2: Network = network_with_seed()
+        network_2.attack_manager.create_black_holes(number_of_black_holes=self.number_of_black_holes, swap_prob=self.swap_prob, targets_per_black_hole=0)
+
+        assert len(network_1.black_holes) == len(network_2.black_holes)
+        assert network_1.black_holes.keys() == network_2.black_holes.keys()
+ 
+        network_1_bha: QuantumRepeater = list(network_1.black_holes.values())[0]
+        network_2_bha: QuantumRepeater = list(network_1.black_holes.values())[0]
+
+        assert (network_1_bha._black_hole_targets is None) and (network_2_bha._black_hole_targets is None)
+        assert network_1_bha._swap_prob == network_2_bha._swap_prob
+
+
+    def test_reproducibility_for_black_hole_with_targets_creation(self) -> None:
+
+        network_1: Network = network_with_seed()
+        network_1.attack_manager.create_black_holes(number_of_black_holes=self.number_of_black_holes, swap_prob=self.swap_prob, targets_per_black_hole=self.targets_per_black_hole)
+
+        network_2: Network = network_with_seed()
+        network_2.attack_manager.create_black_holes(number_of_black_holes=self.number_of_black_holes, swap_prob=self.swap_prob, targets_per_black_hole=self.targets_per_black_hole)
+
+        assert len(network_1.black_holes) == len(network_2.black_holes)
+        assert network_1.black_holes.keys() == network_2.black_holes.keys()
+
+        network_1_bha: QuantumRepeater = list(network_1.black_holes.values())[0]
+        network_2_bha: QuantumRepeater = list(network_1.black_holes.values())[0]
+
+        assert network_1_bha._black_hole_targets == network_2_bha._black_hole_targets
+        assert network_1_bha._swap_prob == network_2_bha._swap_prob
