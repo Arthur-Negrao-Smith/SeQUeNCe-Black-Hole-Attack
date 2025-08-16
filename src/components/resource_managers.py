acb7a6a1e67e81cb3de194e1b44cb35ba2c1c89d
Arthur Negrão <arthurnegrao10s@gmail.com>
2025-08-15 23:41:29 -0300

    style: Use black formatter to organize resource_manager.py.

--- src/components/resource_managers.py
+++ src/components/resource_managers.py
@@ -1,6 +1,9 @@
 from sequence.protocol import Protocol
 from sequence.components.memory import Memory
-from sequence.entanglement_management.swapping import EntanglementSwappingA, EntanglementSwappingB
+from sequence.entanglement_management.swapping import (
+    EntanglementSwappingA,
+    EntanglementSwappingB,
+)
 from sequence.entanglement_management.generation import EntanglementGenerationA
 
 from components.nodes import QuantumRepeater
@@ -14,10 +17,12 @@ import logging
 
 log: logging.Logger = logging.getLogger(__name__)
 
+
 class BaseManager(ABC):
     """
     Base Manager to create to entanglement swapping managers
     """
+
     def __init__(self, owner: QuantumRepeater) -> None:
         self._owner: QuantumRepeater | None = owner
         self.raw_counter: int = 0
@@ -30,7 +35,7 @@ class BaseManager(ABC):
         return self._owner
 
     def update(self, protocol: Type[Protocol], memory: Memory, state: str) -> None:
-        if state == 'RAW':
+        if state == "RAW":
             self.raw_counter += 1
             memory.reset()
         else:
@@ -51,7 +56,9 @@ class RepeaterManager(BaseManager):
     """
     Manager to control entanglement and entanglement swapping in repeater nodes
     """
+
     from .nodes import QuantumRepeater
+
     def __init__(self, owner: QuantumRepeater) -> None:
         super().__init__(owner)
         log.debug(f"Repeater Manager initiated in {self.owner.name}")
@@ -62,28 +69,33 @@ class RepeaterManager(BaseManager):
         """
         left_memo: Memory = self.owner.components[self.owner.left_memo_name]
         right_memo: Memory = self.owner.components[self.owner.right_memo_name]
-        protocol: EntanglementSwappingA = EntanglementSwappingA(self.owner, 
-                                                        f'{self.owner.name}.Entanglement_SwappingA', 
-                                                        left_memo, 
-                                                        right_memo, 
-                                                        self.owner._swap_prob, # type: ignore
-                                                        SWAP_DEGRADATION)
+        protocol: EntanglementSwappingA = EntanglementSwappingA(
+            self.owner,
+            f"{self.owner.name}.Entanglement_SwappingA",
+            left_memo,
+            right_memo,
+            self.owner._swap_prob,  # type: ignore
+            SWAP_DEGRADATION,
+        )
 
-        node_left_name: str | None = left_memo.entangled_memory['node_id']
+        node_left_name: str | None = left_memo.entangled_memory["node_id"]
         # if is entangled
         if node_left_name is not None and node_left_name:
             # if node have targets to atack and left node is a target
-            if (self.owner._black_hole_targets is not None) and (node_left_name in self.owner._black_hole_targets.keys()):
-                protocol = EntanglementSwappingA(self.owner, 
-                                            f'{self.owner.name}.Entanglement_SwappingA', 
-                                            left_memo, 
-                                            right_memo, 
-                                            self.owner._black_hole_targets[node_left_name], # type: ignore
-                                            SWAP_DEGRADATION)
-
+            if (self.owner._black_hole_targets is not None) and (
+                node_left_name in self.owner._black_hole_targets.keys()
+            ):
+                protocol = EntanglementSwappingA(
+                    self.owner,
+                    f"{self.owner.name}.Entanglement_SwappingA",
+                    left_memo,
+                    right_memo,
+                    self.owner._black_hole_targets[node_left_name],  # type: ignore
+                    SWAP_DEGRADATION,
+                )
 
         self.owner.protocols.append(protocol)
-        
+
     def create_swapping_protocolB(self, memory_position: Directions) -> None:
         """
         Create a protocol to perform the entanglement swapping at the side node.
@@ -93,15 +105,21 @@ class RepeaterManager(BaseManager):
         """
         if memory_position == Directions.LEFT:
             left_memo: Memory = self.owner.components[self.owner.left_memo_name]
-            protocol: EntanglementSwappingB = EntanglementSwappingB(self.owner, f'{self.owner.name}.Entanglement_SwappingB', left_memo)
-            self.owner.protocols.append(protocol)
-        
-        elif memory_position == Directions.RIGHT:
-            right_memo: Memory = self.owner.components[self.owner.right_memo_name]
-            protocol: EntanglementSwappingB = EntanglementSwappingB(self.owner, f'{self.owner.name}.Entanglement_SwappingB', right_memo)
+            protocol: EntanglementSwappingB = EntanglementSwappingB(
+                self.owner, f"{self.owner.name}.Entanglement_SwappingB", left_memo
+            )
             self.owner.protocols.append(protocol)
 
-    def create_entanglement_protocol(self, memory_position: Directions, middle_node: str, other_node: str) -> None:
+        elif memory_position == Directions.RIGHT:
+            right_memo: Memory = self.owner.components[self.owner.right_memo_name]
+            protocol: EntanglementSwappingB = EntanglementSwappingB(
+                self.owner, f"{self.owner.name}.Entanglement_SwappingB", right_memo
+            )
+            self.owner.protocols.append(protocol)
+
+    def create_entanglement_protocol(
+        self, memory_position: Directions, middle_node: str, other_node: str
+    ) -> None:
         """
         Create a protocol to perform the entanglement at the side node.
 
@@ -111,9 +129,14 @@ class RepeaterManager(BaseManager):
             other_node (str): RepeaterNode's nanme that will be entangled.
         """
         memory: Memory = self.get_memory(memory_position)
-        
-        protocol: EntanglementGenerationA = EntanglementGenerationA(self.owner, f"{self.owner.name}.Entanglement_GenerationA", 
-                                                                    middle_node, other_node, memory)
+
+        protocol: EntanglementGenerationA = EntanglementGenerationA(
+            self.owner,
+            f"{self.owner.name}.Entanglement_GenerationA",
+            middle_node,
+            other_node,
+            memory,
+        )
         self.owner.protocols.append(protocol)
 
     def get_memory(self, memory_position: Directions) -> Memory:
@@ -140,7 +163,9 @@ class RepeaterManager(BaseManager):
         """
         self.owner._swap_prob = new_swap_prob
 
-    def _turn_black_hole(self, new_swap_prob: float | int, targets: dict[str, float | int] | None) -> None:
+    def _turn_black_hole(
+        self, new_swap_prob: float | int, targets: dict[str, float | int] | None
+    ) -> None:
         """
         Turn the node in a black hole to affect pwapping protocol A
 
@@ -154,7 +179,7 @@ class RepeaterManager(BaseManager):
         if targets is None:
             self._update_swap_prob(new_swap_prob)
             return
-        
+
         # if target's dict doens't exists
         if self.owner._black_hole_targets is None:
             self.owner._black_hole_targets = dict()
